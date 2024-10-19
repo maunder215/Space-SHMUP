7749813001cc12793f4eb5f689eb8ce85cc27137
Marcus Underwood <maunder215@gmail.com>
2024-10-19 06:31:05 -0500

    Finished making the hero ship

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -7,34 +7,45 @@ using UnityEngine;
 /// Keeps a GameObject on screen.
 /// Note that this ONLY works for an orthographic Main Camera.
 /// </summary>
-public class BoundsCheck : MonoBehaviour                // a
+public class BoundsCheck : MonoBehaviour                
 {
+    public enum eType { center, inset, outset };
+
+    [Header("Inscribed")]
+    public eType boundsType = eType.center;
+    public float radius = 1f;
+
     [Header("Dynamic")]
     public float camWidth;
     public float camHeight;
 
     void Awake(){
-        camHeight = Camera.main.orthographicSize;       // b
-        camWidth = camHeight * Camera.main.aspect;      // c
+        camHeight = Camera.main.orthographicSize;       
+        camWidth = camHeight * Camera.main.aspect;      
     }
 
     void LateUpdate(){
-        Vector3 pos = transform.position;                // d
+        // Find the checkRadius that will enable center, inset, or outset
+        float checkRadius = 0;
+        if (boundsType == eType.inset)  checkRadius = -radius;
+        if (boundsType == eType.outset) checkRadius = radius;
+
+        Vector3 pos = transform.position;                
 
         // Restrict the X position to camWidth
-        if (pos.x > camWidth){                          // e
-            pos.x = camWidth;                           // e
+        if (pos.x > camWidth + checkRadius){                          
+            pos.x = camWidth + checkRadius;                           
         }
-        if (pos.x < -camWidth){                         // e    
-            pos.x = -camWidth;                          // e
+        if (pos.x < -camWidth - checkRadius){                             
+            pos.x = -camWidth - checkRadius;                         
         }
 
-        /// Restrict the Y position to camHeight
-        if (pos.y > camHeight){                          // e
-            pos.y = camHeight;                           // e
+        // Restrict the Y position to camHeight
+        if (pos.y > camHeight + checkRadius){                          
+            pos.y = camHeight + checkRadius;                           
         } 
-        if (pos.y < -camHeight){                          // e
-            pos.y = -camHeight;                           // e
+        if (pos.y < -camHeight - checkRadius){                          
+            pos.y = -camHeight - checkRadius;                           
         }
 
         transform.position = pos;
