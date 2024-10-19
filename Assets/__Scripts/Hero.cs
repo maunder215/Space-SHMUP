3cadf2ca5df771d65e2fa4a0ed9298bb685216bb
Marcus Underwood <maunder215@gmail.com>
2024-10-19 09:00:58 -0500

    Making the enemies damage the player

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    static public Hero  S { get; private set; } //Singleton property // a
+    static public Hero  S { get; private set; } //Singleton property 
     [Header("Inscribed")]
     
     //These fields control the movement of the ship
@@ -12,12 +12,14 @@ public class Hero : MonoBehaviour
     public float rollMult = -45;
     public float pitchMult = 30;
 
-    [Header("Dynamic")] [Range(0,4)]                                // b
-    public float        shieldLevel = 1;
+    [Header("Dynamic")] [Range(0,4)]  [SerializeField]                              
+    private float        _shieldLevel = 1;
+    [Tooltip( "This field holds a reference to the last triggering GameObject" )]
+    private GameObject  lastTriggerGo = null;
 
     void Awake() {
         if (S == null) {
-            S = this; // Set the Singleton only if it's null        // c
+            S = this; // Set the Singleton only if it's null        
         } else {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
@@ -27,8 +29,8 @@ public class Hero : MonoBehaviour
     void Update()
     {
         // Pull in information from the Input class             
-        float hAxis = Input.GetAxis("Horizontal");              // d
-        float vAxis = Input.GetAxis("Vertical");                // d
+        float hAxis = Input.GetAxis("Horizontal");              
+        float vAxis = Input.GetAxis("Vertical");               
 
         // Change transform.position based on the axes
         Vector3 pos = transform.position;
@@ -36,7 +38,37 @@ public class Hero : MonoBehaviour
         pos.y += vAxis * speed * Time.deltaTime;
         transform.position = pos;
 
-        // Rotate the ship to make it feel more dynamic          // e
+        // Rotate the ship to make it feel more dynamic         
         transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
     }
+
+    void OnTriggerEnter(Collider other) {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        // Debug.Log("Shield trigger hit by: " + go.gameObject.name);
+
+        // Make sure it's not the same triggering go as last time
+        if (go == lastTriggerGo ) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null) {  // If the shield was triggered by an enemy
+            shieldLevel--;   // Decrease the level of the shield by 1
+            Destroy(go);     // ... and Destroy the enemy
+        } else {
+            Debug.LogWarning("Shield trigger hit by non-Enemy: " + go.name);
+        }
+    }
+
+    public float shieldLevel {
+        get { return _shieldLevel; }
+        private set {
+            _shieldLevel = Mathf.Min (value, 4 );
+            // If the shield is going to be set to less than zero...
+            if ( value < 0 ) {
+                Destroy(this.gameObject);   // Destroy the Hero
+            }
+        }
+        }
+    
 }
