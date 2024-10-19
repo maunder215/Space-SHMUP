9ac1e587acfb10caecf08651c3ba5ac611a823c2
Marcus Underwood <maunder215@gmail.com>
2024-10-19 14:38:07 -0500

    Almost finished enemy 3

--- Assets/__Scripts/Enemy_3.cs
+++ Assets/__Scripts/Enemy_3.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_3 : MonoBehaviour
+public class Enemy_3 : Enemy            // Enemy_3 also extends the Enemy class
 {
+    [Header("Enemy_3 Inscribed Fields")]
+    public float lifeTime = 5;
+    public Vector2 midpointYRange = new Vector2(1.5f, 3);
+    [Tooltip("If true, the Bezier points & path are drawn in the Scene pane.")]
+    public bool drawDebugInfo = true;
     // Start is called before the first frame update
     void Start()
     {
