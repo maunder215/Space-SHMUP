087917d5e162fe56177c9f525208de0075245254
Marcus Underwood <maunder215@gmail.com>
2024-10-19 07:20:44 -0500

    Adding some enemies

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -9,13 +9,24 @@ using UnityEngine;
 /// </summary>
 public class BoundsCheck : MonoBehaviour                
 {
+    [System.Flags]
+    public enum eScreenLocs {
+        onScreen = 0,       // 0000 in binary (zero)
+        offRight = 1,       // 0001 in binary
+        offLeft = 2,        // 0010 in binary
+        offUp = 4,          // 0100 in binary
+        offDown = 8         // 1000 in binary
+    }
     public enum eType { center, inset, outset };
 
     [Header("Inscribed")]
     public eType boundsType = eType.center;
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Dynamic")]
+    public eScreenLocs screenLocs = eScreenLocs.onScreen;
+    // public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
 
@@ -30,35 +41,51 @@ public class BoundsCheck : MonoBehaviour
         if (boundsType == eType.inset)  checkRadius = -radius;
         if (boundsType == eType.outset) checkRadius = radius;
 
-        Vector3 pos = transform.position;                
+        Vector3 pos = transform.position; 
+        screenLocs = eScreenLocs.onScreen;  
+        // isOnScreen = true;             
 
         // Restrict the X position to camWidth
         if (pos.x > camWidth + checkRadius){                          
-            pos.x = camWidth + checkRadius;                           
+            pos.x = camWidth + checkRadius;
+            screenLocs |= eScreenLocs.offRight;                        
+            // isOnScreen = false;
         }
         if (pos.x < -camWidth - checkRadius){                             
-            pos.x = -camWidth - checkRadius;                         
+            pos.x = -camWidth - checkRadius;  
+            screenLocs |= eScreenLocs.offLeft;                       
+            // isOnScreen = false;
         }
 
         // Restrict the Y position to camHeight
         if (pos.y > camHeight + checkRadius){                          
-            pos.y = camHeight + checkRadius;                           
+            pos.y = camHeight + checkRadius;  
+            screenLocs |= eScreenLocs.offUp;                         
+            // isOnScreen = false;
         } 
         if (pos.y < -camHeight - checkRadius){                          
-            pos.y = -camHeight - checkRadius;                           
+            pos.y = -camHeight - checkRadius;  
+            screenLocs |= eScreenLocs.offDown;                         
+            // isOnScreen = false;
+        }
+
+        if ( keepOnScreen && !isOnScreen ) {
+            transform.position = pos;
+            screenLocs = eScreenLocs.onScreen;
+            // isOnScreen = true;
         }
 
         transform.position = pos;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public bool isOnScreen {
+        get {
+            return ( screenLocs == eScreenLocs.onScreen );
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public bool LocIs ( eScreenLocs checkLoc ) {
+        if ( checkLoc == eScreenLocs.onScreen ) return isOnScreen;
+        return ( (screenLocs & checkLoc ) == checkLoc );
     }
 }
